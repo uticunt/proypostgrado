2f92164cf3abb51d4994588eca76fab8f226b9ef
uticunt <upg_utic@unitru.edu.pe>
2024-04-26 11:48:40 -0500

    Actualizacion de estado

--- proypostgrado/settings.py
+++ proypostgrado/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5d2+i3hg#wlnx3k&noomp@=sn81p4^@lhy$@#gxs)3&e+z!d+v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','*']
 
 
 # Application definition
