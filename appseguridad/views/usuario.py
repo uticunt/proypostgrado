794638db16324434d2617c5a5be5e6dd86c1291f
Mishaelrojas <mjrojasv@unitru.edu.pe>
2024-04-27 13:00:28 -0500

    Cambios en listar usuarios

--- appseguridad/views/usuario.py
+++ appseguridad/views/usuario.py
@@ -18,21 +18,12 @@ def listar_usuarios(request):
     queryset = request.GET.get("buscar")
     datos = User.objects.all()    
     # Conteo
-    conteo = datos.count()
-    # Desencriptar contraseñas
-    if settings.DEBUG:
-        contraseñas_desencriptadas = []
-        for usuario in datos:
-            contraseña_desencriptada = usuario.password if check_password('', usuario.password) else 'No se puede desencriptar'
-            contraseñas_desencriptadas.append(contraseña_desencriptada)
-    else:
-        contraseñas_desencriptadas = ['*****' for _ in datos]  # Oculta las contraseñas en producción
+    conteo = datos.count()   
 
     datos = datos.order_by('id')    
     paginator = Paginator(datos, 10)  
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number) 
+    
 
-    usuarios_contraseñas = zip(page_obj, contraseñas_desencriptadas)
-
-    return render(request, 'usuario/listar.html', {'page_obj': page_obj, 'conteo': conteo, 'usuarios_contraseñas': usuarios_contraseñas})
+    return render(request, 'usuario/listar.html', {'page_obj': page_obj, 'conteo': conteo})
